f87a45633090b483b6333050ed1ccc4014da19e6
Glitch (stark-code-bos-altyapi) <none>
2020-02-29 14:02:13 +0000

    💆🍃 Checkpoint ./bot.js:9181222/2 ./komutlar/adamasmaca.js:9181222/3400 ./komutlar/eteşet.js:9181222/1769 ./komutlar/yardım.js:9181222/2257 ./ayarlar.json:9181222/204 ./komutlar/aşkölçer.js:9181222/2092 ./komutlar/balıktut.js:9181222/959 ./komutlar/duello.js:9181222/5398 ./komutlar/efkar.js:9181222/369 ./komutlar/emojiler.js:9181222/1206 ./komutlar/emojiyazı.js:9181222/1830 ./komutlar/hapishane.js:9181222/1234 ./komutlar/hesapla.js:9181222/856 ./komutlar/kartopu.js:9181222/1178 ./komutlar/karıştır.js:9181222/630

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,46 +1,51 @@
-const Discord = require("discord.js");
-const moment = require("moment");
-require("moment-duration-format");
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
 
-exports.run = (client, msg) => {
-  msg.channel.sendCode("asciidoc", `=YARDIM MENÜSÜ=
-• {prefix}adamasmaca :: Adam asmaca oynarsınız
-• {prefix}ateş-et @etiket :: Etiketlediğin kişiye ateş açar
-• {prefix}avatar :: Profil resmini atar
-• {prefix}aşkölçer @etiket :: Kişiye olan aşkını ölçer
-• {prefix}balıktut :: Şansa balık tutarsınız
-• {prefix}ban @etiket :: Kullanıcıya ban atarsınız
-• {prefix}düello @etiket :: Kişiye düello atar
-• {prefix}efkarım :: Efkarını Ölçer
-• {prefix}emojiler :: Sunucudaki emojileri atar
-• {prefix}emojiyazı <yazı> :: Emojilerle yazı yazarsınız
-• {prefix}fakemesaj @etiket <yazı> :: Etiketlediğin kişinin adına mesaj atar
-• {prefix}hapishane :: Profilinize hapishane efekti ekler
-• {prefix}hesapla <işlem> :: Yazdığınız işlemi hesaplar
-• {prefix}kartopu :: Kartopu atarsınız
-• {prefix}karıştır <yazı> :: Yazdığınız yazıdaki harfleri karıştırır
-• {prefix}kick @etiket :: Kişiyi sunucudan atar
-• {prefix}ping :: Botunun pingini gösterir
-• {prefix}pixel :: Profilini bulanık yapar
-• {prefix}reklamtaraması :: Chati temizler
-• {prefix}rip :: Profilinize rip efekti ekler
-• {prefix}saat :: Türkiyenin saatini gösterir
-• {prefix}servericon :: Sunucunun profil resmini gösterir
-• {prefix}slowmode 1-10 :: Sohbete yavaşlık ekler
-• {prefix}stresçarkı :: Stres çarkı çevirir
-• {prefix}bayrak :: Profiline TÜRK BAYRAĞI efekti ekler
-• {prefix}wasted :: Profiline wasted efekti ekler`);
+let botid = ('672381245115727911') //bu yere botun id'sini yapıştırın.
+//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
+
+exports.run = (client, message, args) => {
+    const embed = new Discord.RichEmbed()
+        .setAuthor(`${client.user.username} `, client.user.avatarURL)
+        .setColor('#00ff1f')
+        .setTitle(`${client.user.username} - Komutlar`)
+        .setDescription(`<a:di:681084642371305582>**cr!adamasmaca** Adam asmaca oynarsınız
+<a:di:681084642371305582>**cr!ateş-et @etiket** Etiketlediğin kişiye ateş açar
+<a:di:681084642371305582>**cr!aşkölçer @etiket** Kişiye olan aşkını ölçer
+<a:di:681084642371305582>**cr!balıktut** Şansa balık tutarsınız
+<a:di:681084642371305582>**cr!düello @etiket** Kişiye düello atar
+<a:di:681084642371305582>**cr!efkarım** Efkarını Ölçer
+<a:di:681084642371305582>**cr!emojiler** Sunucudaki emojileri atar
+<a:di:681084642371305582>**cr!emojiyazı <yazı>** Emojilerle yazı yazarsınız
+<a:di:681084642371305582>**cr!hapishane** Profilinize hapishane efekti ekler
+<a:di:681084642371305582>**cr!hesapla <işlem>** Yazdığınız işlemi hesaplar
+<a:di:681084642371305582>**cr!kartopu** Kartopu atarsınız
+<a:di:681084642371305582>**cr!karıştır <yazı>** Yazdığınız yazıdaki harfleri karıştırır
+<a:di:681084642371305582>**cr!pixel** Profilini bulanık yapar
+<a:di:681084642371305582>**cr!reklamtaraması** Chati temizler
+<a:di:681084642371305582>**cr!rip** Profilinize rip efekti ekler
+<a:di:681084642371305582>**cr!saat** Türkiyenin saatini gösterir
+<a:di:681084642371305582>**cr!stresçarkı** Stres çarkı çevirir
+<a:di:681084642371305582>**cr!bayrak** Profiline TÜRK BAYRAĞI efekti ekler
+<a:di:681084642371305582>**cr!wasted** Profiline wasted efekti ekler
+<a:di:681084642371305582>**cr!kaçcm** Uzunluk Ölçer :D`)  
+        .setThumbnail(client.user.avatarURL)
+        .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.avatarURL)
+        .setImage("")  
+    return message.channel.sendEmbed(embed);
+  
+  
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['yardım', 'youtube', 'yardım menüsü', 'help'],
-  permLevel: 0
+  aliases: ['eğlen','eglence'],
+  permLevel: 0,
 };
 
 exports.help = {
-  name: 'yardım',
-  description: 'Botun istatistik gösterir.',
-  usage: 'istatistik'
+  name: 'eğlence',
+  description: '',
+  usage: ''
 };
