005e0015082b409655b9101f965421cf5776a1e6
Glitch (stark-code-bos-altyapi) <none>
2020-02-29 16:06:54 +0000

    🎉🐊 Checkpoint ./komutlar/yardım.js:9181222/99

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,7 +9,7 @@ exports.run = (client, message, args) => {
         .setAuthor(`${client.user.username} `, client.user.avatarURL)
         .setColor('#00ff1f')
         .setTitle(`<a:te:681510124178767892>${client.user.username} - Komutlar<a:te2:681510124568707084>`)
-        .setDescription(`<a:di:681084642371305582>**${ayarlar.prefix}yetkili** Yetkili Komutları.\n <a:di:681084642371305582>**${ayarlar.prefix}kullanıcı** Kullanıcıya Komutları.\n ` + `<a:di:681084642371305582>**${ayarlar.prefix}seviyeyardım**  Seviyeyardım Komutları Gösterir.\n` + `<a:di:681084642371305582>**${ayarlar.prefix}eğlence** Eğlence Komutlarını Gösterir.\n` + `<a:di:681084642371305582>**${ayarlar.prefix}müzik** Müzik Komutlarını Gösterir.\n <a:di:681084642371305582>**${ayarlar.prefix}bot** Bot Komutlarını Gösterir.\n <a:8299_Loading1:608249368386207751>**${ayarlar.prefix}vip** Bakım :outbox_tray:\n`)  
+        .setDescription(`<a:di:681084642371305582>**${ayarlar.prefix}yetkili** Yetkili Komutları.\n <a:di:681084642371305582>**${ayarlar.prefix}kullanıcı** Kullanıcıya Komutları.\n ` + `<a:di:681084642371305582>**${ayarlar.prefix}eğlence** Eğlence Komutlarını Gösterir.\n` + `<a:di:681084642371305582>**${ayarlar.prefix}müzik** Müzik Komutlarını Gösterir.\n <a:di:681084642371305582>**${ayarlar.prefix}bot** Bot Komutlarını Gösterir.\n <a:8299_Loading1:608249368386207751>**${ayarlar.prefix}vip** Bakım :outbox_tray:\n`)  
         .setThumbnail(client.user.avatarURL)
         .addField(`» Linkler`, `
 [Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=681938616611897365&scope=bot&permissions=2146958847)
