b0f2853d59f8c707d1f2fd6e197ea27da59e764f
Glitch (stark-code-bos-altyapi) <none>
2020-03-19 19:22:04 +0000

    🌔⛽️ Checkpoint ./bot.js:9181222/1115 ./komutlar/anti-spam.js:9181222/1164 ./package.json:9181222/4304 ./spamEngel.json:9181222/2

--- bot.js
+++ bot.js
@@ -787,6 +787,8 @@ client.on("message", async msg => {
 //-----------------------SohbetKapat Son-----------------------\\
 
 
+
+
 client.on('channelCreate', async channel => {
   const c = channel.guild.channels.get(db.fetch(`codeminglog_${channel.guild.id}`));
   if (!c) return;
@@ -987,3 +989,30 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
     
   }
 });
+
+
+
+const antispam = require("discord-anti-spam-tr");
+client.on("message", msg => {
+  const spamEngel = db.get(`spamEngel_${msg.guild.id}`)
+
+  if(spamEngel == "açık"){
+
+
+let spamEngel = JSON.parse(fs.readFileSync("./ayarlar/spamEngel.json", "utf8"));
+//istediğiniz yere ekleyin bot.js de
+
+antispam(client, {
+  uyarmaSınırı: 15, //Uyarılmadan önce aralıkta gönderilmesine izin verilen maksimum mesaj miktarı.
+  banlamaSınırı: 20, //Yasaklanmadan önce aralıkta gönderilmesine izin verilen maksimum ileti miktar.
+  aralık: 300000, // ms kullanıcılarda zaman miktarı, yasaklanmadan önce aralık değişkeninin maksimumunu gönderebilir.
+  // Uyarı mesajı, kullanıcıya hızlı gideceklerini belirten kullanıcıya gönderilir..
+   //Yasak mesaj, yasaklanmış kullanıcıyı ,Banlar
+  maxSpamUyarı: 7,//Bir kullanıcının uyarılmadan önce bir zaman dilimi içinde gönderebileceği maksimum kopya sayısı
+  maxSpamBan: 20, //Bir kullanıcının yasaklanmadan önce bir zaman diliminde gönderebildiği maksimum kopya sayısı
+  zaman: 7, // Spamdan sonraki zaman
+  rolİsimi: "spam-susturulmuş" // Spam Atan Kullanıcılar Verilecek Röl
+})
+};
+    }
+)
