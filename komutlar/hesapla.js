94b3b295c4575135c3157aa2c8e49ad12f963e5e
Glitch (stark-code-bos-altyapi) <none>
2020-03-20 22:22:30 +0000

    📺🎥 Checkpoint ./komutlar/hesapla.js:11195635/2090

--- komutlar/hesapla.js
+++ komutlar/hesapla.js
@@ -1,28 +1,29 @@
+
 const Discord = require('discord.js');
 const math = require('math-expression-evaluator')
 const stripIndents = require('common-tags').stripIndents
+const db = require('quick.db');
 
 
-
-exports.run = function(client, message, args) {
+exports.run = async(client, message, args) => {
     var soru = args.join(' ');
-    
-    if(!soru) return message.reply('Bir işlem belirtin. **Doğru Kullanım**: -hesapla <işlem>')
-    else { let cevap;
+    if(!soru) return message.reply('Bir işlem belirtin. `-hesapla <işlem>`')
+    else {
         try {
-            cevap = math.eval(soru)
-        } catch(err) {
-            message.channel.send('Hatalı işlem: **' + err)
-        }
-
+        let cevap = math.eval(soru)
         const embed = new Discord.RichEmbed()
+        .setColor("#FFB900")
+        .setAuthor("Bot Hesaplama Sistemi", client.user.avatarURL)
+        .setFooter(message.author.username+' tarafından istendi.', message.author.avatarURL || message.author.defaultAvatarURL)
         .addField('Soru', soru)
         .addField('Cevap', cevap)
 
         message.channel.send(embed)
+        } catch(err) {
+          return message.channel.send('Hatalı **işlem.**')
+        }
     }
-
-
+                                                        
 };  
 
 exports.conf = {
