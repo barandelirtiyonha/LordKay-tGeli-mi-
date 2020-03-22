82183bc35902b582173d07ca3264ee7c3eaa9fd3
Glitch (stark-code-bos-altyapi) <none>
2020-03-22 11:33:13 +0000

    🎊🏕 Checkpoint ./komutlar/sil.js:11195635/843

--- komutlar/sil.js
+++ komutlar/sil.js
@@ -1,9 +1,10 @@
+
 const Discord = require('discord.js');
 exports.run = function(client, message, args) {
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bu Komutu Kullanmak İçin İzniniz Yok!");
 if(!args[0]) return message.channel.send("🚫 **Lütfen Silinicek Mesaj Miktarını Yazın.!** 🚫");
 message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(` ${args[0]} Adet Mesajı Sildim.`).then(msg => msg.delete(5000));
+  message.channel.send(` ${args[0]} Adet Mesajı Sildim. ✅`).then(msg => msg.delete(5000));
 })
 }
 
