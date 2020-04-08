ef2685235a0fadc7074465fd98cc693a75fb415a
Glitch (stark-code-bos-altyapi) <none>
2020-04-08 16:26:20 +0000

    🚶🚃 Checkpoint ./events/guildMemberRemove.js:11195635/70 ./events/guildMemberAdd.js:11195635/72 ./komutlar/erkekkayıt.js:11195635/1739 ./komutlar/kızkayıt.js:11195635/1741 ./bot.js:11195635/18

--- bot.js
+++ bot.js
@@ -188,12 +188,12 @@ client.on("guildMemberAdd", async (member) => {
 
 //Sunucuya biri girdiğinde kanal ismi değiştirme
 client.on("guildMemberAdd", message => {
-  client.channels.get("697184746425417788").setName(`Kişi Sayısı: ${message.guild.memberCount} 📤`);
+  client.channels.get("697184746425417788").setName(`Toplam Kişi Sayısı: ${message.guild.memberCount} `);
 // kanal id yazan yerlere sesli kanalın id'sini sağtıklayıp kopyalayın ve yapıştırın
 });
 //Sunucudan Çıktığın Kişi Sayını Azaltma
 client.on("guildMemberRemove", message => {
-  client.channels.get("697184746425417788").setName(`Kişi Sayısı: ${message.guild.memberCount} 📥`);
+  client.channels.get("697184746425417788").setName(`Toplam Kişi Sayısı: ${message.guild.memberCount} `);
   
 });
 
